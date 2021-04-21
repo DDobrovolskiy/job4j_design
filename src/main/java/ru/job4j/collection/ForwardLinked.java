14eccd903ec928c41394e7230a97366dc802881b
DmitriyDobrovolskiy <dda.90@mail.ru>
2021-04-21 22:26:02 +0500

    4. Используя контейнер на базе связанного списка создать контейнер Stack [#521651]

--- src/main/java/ru/job4j/collection/ForwardLinked.java
+++ src/main/java/ru/job4j/collection/ForwardLinked.java
@@ -1,22 +1,39 @@
 package ru.job4j.collection;
 
+import java.util.ConcurrentModificationException;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
 public class ForwardLinked<T> implements Iterable<T> {
     private Node<T> head;
+    private int countMod = 0;
+    private int size;
 
     public void add(T value) {
         Node<T> node = new Node<T>(value, null);
         if (head == null) {
             head = node;
+        } else {
+            Node<T> tail = head;
+            while (tail.next != null) {
+                tail = tail.next;
+            }
+            tail.next = node;
+        }
+        countMod++;
+        size++;
+    }
+
+    public void addFirst(T value) {
+        Node<T> node = new Node<T>(value, null);
+        if (head == null) {
+            head = node;
             return;
         }
-        Node<T> tail = head;
-        while (tail.next != null) {
-            tail = tail.next;
-        }
-        tail.next = node;
+        node.next = head;
+        head = node;
+        countMod++;
+        size++;
     }
 
     public T deleteFirst() {
@@ -25,6 +42,8 @@ public class ForwardLinked<T> implements Iterable<T> {
         }
         Node<T> delete = head;
         head = head.next;
+        countMod++;
+        size--;
         return delete.value;
     }
 
@@ -32,9 +51,13 @@ public class ForwardLinked<T> implements Iterable<T> {
     public Iterator<T> iterator() {
         return new Iterator<T>() {
             Node<T> node = head;
+            private int countModExpected = countMod;
 
             @Override
             public boolean hasNext() {
+                if (countMod != countModExpected) {
+                    throw new ConcurrentModificationException();
+                }
                 return node != null;
             }
 
