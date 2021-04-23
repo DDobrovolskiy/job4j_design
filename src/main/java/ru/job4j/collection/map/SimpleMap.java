a09fbc546d4dc9dce5e6c109a6a8c644c99effdb
DmitriyDobrovolskiy <dda.90@mail.ru>
2021-04-23 17:47:06 +0500

    6. Коллизии Map [#521685]

---
+++ src/main/java/ru/job4j/collection/map/SimpleMap.java
@@ -0,0 +1,25 @@
+package ru.job4j.collection.map;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.HashMap;
+import java.util.Map;
+
+public class SimpleMap {
+    public static void main(String[] args) {
+        Calendar birthday = new GregorianCalendar();
+        birthday.set(1990, 10, 10);
+
+        User user1 = new User("Name", 1, birthday);
+        User user2 = new User("Name", 1, birthday);
+
+        System.out.println(user1.hashCode());
+        System.out.println(user2.hashCode());
+
+        Map<User, Object> map = new HashMap<>();
+        map.put(user1, new Object());
+        map.put(user2, new Object());
+        System.out.println(map);
+
+    }
+}
