39fed095ad5bc8330448403b68173fbdfeb99541
stGOST <dda.90@mail.ru>
2021-05-02 12:21:04 +0500

    1. Экзамен. Структуры данных и алгоритмы. [#521647]

--- src/main/java/ru/job4j/collection/ex/UserEmail.java
+++ src/main/java/ru/job4j/collection/ex/UserEmail.java
@@ -4,52 +4,107 @@ import java.util.*;
 
 public class UserEmail {
 
-    public Map<String, List<String>> mergeUsersOnEmail(Map<String, List<String>> users) {
-        Map<String, List<String>> mergeUsers = new HashMap<>(users.size());
-        Node nodes = null;
+    private Map<String, List<String>> mergeUsers;
+    private Map<String, Node> uniqueUser = new HashMap<>();
+    private Map<String, Node> uniqueEmail = new HashMap<>();
+
+    public Map<String, List<String>> mergeUser(Map<String, List<String>> users) {
+        mergeUsers = new HashMap<>(users.size());
         for (Map.Entry<String, List<String>> user : users.entrySet()) {
-            if (nodes == null) {
-                nodes = new Node(user.getKey(), user.getValue());
-            } else {
-                nodes.merge(user.getKey(), user.getValue());
+            //O(n)
+            for (String email : user.getValue()) {
+                //O(n + m)
+                add(user.getKey(), email);
             }
-        }
-        while (nodes != null) {
-            mergeUsers.put(nodes.nameNode, new ArrayList<>(nodes.emailsNode));
-            nodes = nodes.nextNode;
         }
         return mergeUsers;
     }
 
-    private static class Node {
-        private String nameNode;
-        private Set<String> emailsNode;
-        private Node nextNode;
+    private void add(String user, String email) {
+        if (uniqueEmail.containsKey(email)) {
+                //Объединить двух узеров у кого есть такой емайл
+            merge(user, email);
+        } else {
+                //Вставить к юзеру емайл, так как пока емайл уникальный
+            insert(user, email);
+        }
+    }
 
-        public Node(String name, Collection<String> emails) {
-            this.nameNode = name;
-            this.emailsNode = new HashSet<>(emails);
+    private void merge(String user, String email) {
+        Node nodeEmail = uniqueEmail.get(email); //По емайлу узнаем у кого есть такой же емайл
+        if (uniqueUser.containsKey(user)) {
+            //Если такой юзер есть, вытаскиваем его запись
+            Node nodeUser = uniqueUser.get(user);
+            //Проверяем если записи равны, то ничего делать не надо,
+            //так как в этом юзере уже есть нужный емал
+            //если нет, значит в Map mergeUser присутствуют записи, которые необходимо слить
+            if (!nodeUser.equals(nodeEmail)) {
+                mergeInMapMergeUser(nodeUser.name, nodeEmail.name);
+            }
+        } else {
+            uniqueUser.put(user, nodeEmail); //Если такой юзер еще не пресутствовал добавить в список
+            //с ссылкой на юзера у которого есть такой емайл
+            //емайл вставлять не надо так как он есть у этого юзера
+        }
+    }
+
+    private void mergeInMapMergeUser(String user1, String user2) {
+        //Сливаем емайлы юзеров
+        mergeUsers.merge(user1, mergeUsers.get(user2), (email1, email2) -> {
+            email1.addAll(email2);
+            return email1;
+        });
+        mergeUsers.remove(user2); //Удаляем юзера2 из итогового списка
+        Node nodeUser1 = uniqueUser.get(user1); //Ссылка на юзера 1
+        uniqueUser.put(user2, nodeUser1); //Переписываем ссылку юзера 2 на юзер 1
+    }
+
+    private void insert(String user, String email) {
+        //Получаем или создаем запись на юзера
+        Node node = getNode(user);
+        //Добавляем к списку уникальных емайлов запись
+        uniqueEmail.put(email, node);
+        //Вставляем в итоговый список емайл
+        insertInMapMergeUser(node.name, email);
+    }
+
+    private void insertInMapMergeUser(String user, String email) {
+        if (mergeUsers.containsKey(user)) {
+            mergeUsers.get(user).add(email);
+        } else {
+            mergeUsers.put(user, new ArrayList<>(List.of(email)));
+        }
+    }
+
+    private Node getNode(String user) {
+        if (!uniqueUser.containsKey(user)) {
+            uniqueUser.put(user, new Node(user));
+        }
+        return uniqueUser.get(user);
+    }
+
+    static class Node {
+        String name;
+
+        Node(String name) {
+            this.name = name;
         }
 
-        public void merge(String name, Collection<String> emails) {
-            if (containsAny(emails)) {
-                this.emailsNode.addAll(emails);
-            } else {
-                if (nextNode == null) {
-                    nextNode = new Node(name, emails);
-                } else {
-                    nextNode.merge(name, emails);
-                }
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) {
+                return true;
             }
+            if (o == null || getClass() != o.getClass()) {
+                return false;
+            }
+            Node node = (Node) o;
+            return Objects.equals(name, node.name);
         }
 
-        private boolean containsAny(Collection<String> emails) {
-            for (String email : emails) {
-                if (this.emailsNode.contains(email)) {
-                    return true;
-                }
-            }
-            return false;
+        @Override
+        public int hashCode() {
+            return Objects.hash(name);
         }
     }
 }
--- src/test/java/ru/job4j/collection/ex/UserEmailTest.java
+++ src/test/java/ru/job4j/collection/ex/UserEmailTest.java
@@ -9,34 +9,61 @@ import java.util.Map;
 import java.util.Set;
 
 import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.*;
 
 public class UserEmailTest {
+
+    @Test
+    public void mergeUsersUniqueEmailAndUser() {
+        Map<String, List<String>> users = new HashMap<>();
+
+        users.put("user0", List.of("1@one.net"));
+        users.put("user1", List.of("xxx@ya.ru", "foo@gmail.com", "lol@mail.ru"));
+
+        UserEmail userEmail = new UserEmail();
+        Map<String, List<String>> actual = userEmail.mergeUser(users);
+
+        for (Map.Entry<String, List<String>> item : actual.entrySet()) {
+            if (Set.of("user1").contains(item.getKey())) {
+                Assert.assertTrue(
+                        item.getValue()
+                                .containsAll(Set.of("xxx@ya.ru", "foo@gmail.com", "lol@mail.ru")));
+                continue;
+            }
+            if (Set.of("user0").contains(item.getKey())) {
+                Assert.assertTrue(
+                        item.getValue()
+                                .containsAll(Set.of("1@one.net")));
+                continue;
+            }
+            Assert.assertTrue(false);
+        }
+    }
 
     @Test
     public void mergeUsersOnEmail() {
         Map<String, List<String>> users = new HashMap<>();
 
+        users.put("user0", List.of("1@one.net"));
         users.put("user1", List.of("xxx@ya.ru", "foo@gmail.com", "lol@mail.ru"));
         users.put("user2", List.of("foo@gmail.com", "ups@pisem.net"));
-        users.put("user3", List.of("xyz@pisem.net", "vasya@pupkin.com"));
+        users.put("user3", List.of("xyz@pisem.net", "vasya@pupkin.com", "1@one.net"));
         users.put("user4", List.of("ups@pisem.net", "aaa@bbb.ru"));
         users.put("user5", List.of("xyz@pisem.net"));
 
         UserEmail userEmail = new UserEmail();
-        Map<String, List<String>> actual = userEmail.mergeUsersOnEmail(users);
+        Map<String, List<String>> actual = userEmail.mergeUser(users);
 
         for (Map.Entry<String, List<String>> item : actual.entrySet()) {
             if (Set.of("user1", "user2", "user4").contains(item.getKey())) {
                 Assert.assertTrue(
-                        Set.of("xxx@ya.ru", "foo@gmail.com", "lol@mail.ru", "ups@pisem.net", "aaa@bbb.ru")
-                                .containsAll(item.getValue()));
+                        item.getValue()
+                                .containsAll(Set.of("xxx@ya.ru", "foo@gmail.com", "lol@mail.ru", "ups@pisem.net", "aaa@bbb.ru")));
                 continue;
             }
-            if (Set.of("user3", "user5").contains(item.getKey())) {
+            if (Set.of("user3", "user5", "user0").contains(item.getKey())) {
                 Assert.assertTrue(
-                        Set.of("xyz@pisem.net", "vasya@pupkin.com")
-                                .containsAll(item.getValue()));
+                        item.getValue()
+                        .containsAll(Set.of("xyz@pisem.net", "vasya@pupkin.com", "1@one.net")));
                 continue;
             }
             Assert.assertTrue(false);
