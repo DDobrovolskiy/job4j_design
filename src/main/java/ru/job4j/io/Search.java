1f22eb31a3ea6e33801f65eece0895de1444fdec
stGOST <dda.90@mail.ru>
2021-05-04 13:40:10 +0500

    5. Валидация параметров запуска. [#521699]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -11,8 +11,11 @@ import static java.nio.file.FileVisitResult.CONTINUE;
 
 public class Search {
     public static void main(String[] args) throws IOException {
-        Path start = Paths.get(".");
-        search(start, p -> p.toFile().getName().endsWith(".java")).forEach(System.out::println);
+        if (args.length != 2) {
+            throw new IllegalArgumentException("Некоректные параметры запуска. Используйте java -jar dir.jar ROOT_FOLDER TYPE_FILE");
+        }
+        Path start = Paths.get(args[0]);
+        search(start, p -> p.toFile().getName().endsWith(args[1])).forEach(System.out::println);
     }
 
     public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
