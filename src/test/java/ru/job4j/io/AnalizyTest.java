81dae44ec2b979a73ee7431547841963d435393c
DmitriyDobrovolskiy <dda.90@mail.ru>
2021-05-06 17:54:24 +0500

    3.0. Тестирование IO [#521698]

--- src/test/java/ru/job4j/io/AnalizyTest.java
+++ src/test/java/ru/job4j/io/AnalizyTest.java
@@ -22,11 +22,11 @@ public class AnalizyTest {
         File target = folder.newFile("target.txt");
 
         try (PrintWriter out = new PrintWriter(source.getAbsoluteFile())) {
-            out.print("200 10:56:01\n"
-                    + "500 10:57:01\n"
-                    + "400 10:58:01\n"
-                    + "200 10:59:01\n"
-                    + "500 11:01:02\n"
+            out.print("200 10:56:01" + System.lineSeparator()
+                    + "500 10:57:01" + System.lineSeparator()
+                    + "400 10:58:01" + System.lineSeparator()
+                    + "200 10:59:01" + System.lineSeparator()
+                    + "500 11:01:02" + System.lineSeparator()
                     + "200 11:02:02");
         }
 
