9fe3cabe8047ae6caac96ec44e8eaf72ee013224
stGOST <dda.90@mail.ru>
2021-05-11 20:23:25 +0500

    1. Бот [#521723]

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -1,37 +1,36 @@
 package ru.job4j.io;
 
 import java.io.*;
-import java.lang.reflect.Array;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.Arrays;
 
 public class EchoServer {
-    private static boolean closeServer = false;
-
     public static void main(String[] args) throws IOException {
         try (ServerSocket server = new ServerSocket(9000)) {
             while (!server.isClosed()) {
                 Socket socket = server.accept();
-                try (OutputStream out = socket.getOutputStream();
+                try (BufferedWriter out = new BufferedWriter(
+                        new OutputStreamWriter(socket.getOutputStream()));
                      BufferedReader in = new BufferedReader(
                              new InputStreamReader(socket.getInputStream()))) {
-                    String str = in.readLine();
-                    while (!str.isEmpty()) {
-                        System.out.println(str);
-                        Arrays.asList(str.split(" "))
-                                .forEach(msg -> {
-                                    if (msg.equals("/?msg=Bye")) {
-                                        try {
-                                            server.close();
-                                        } catch (IOException e) {
-                                            e.printStackTrace();
-                                        }
-                                    }
-                                });
-                        str = in.readLine();
+                    System.out.println("Client connect...");
+                    String str = in.readLine().split(" ")[1];
+                    String serverMessage = "What?";
+                    if (str.startsWith("/?msg=")) {
+                        if (str.equals("/?msg=Exit")) {
+                            try {
+                                serverMessage = "Close server...";
+                                server.close();
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                        } else if (str.equals("/?msg=Hello")) {
+                            serverMessage = "Hello";
+                        }
                     }
-                    out.write("HTTP/1.1 200 OK\r\n\\".getBytes());
+                    out.write("HTTP/1.1 200 OK\r\n\r\n");
+                    out.write(serverMessage);
+                    out.flush();
                 }
             }
         }
