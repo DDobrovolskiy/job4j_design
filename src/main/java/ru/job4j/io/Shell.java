36d0817d5160fbc3f52b2f6e9ad9373d92e6e54a
stGOST <dda.90@mail.ru>
2021-05-28 18:10:30 +0500

    1. Экзамен. Ввод-вывод. [#571 #158730]

--- src/main/java/ru/job4j/io/Shell.java
+++ src/main/java/ru/job4j/io/Shell.java
@@ -6,16 +6,22 @@ import java.util.regex.Pattern;
 
 public class Shell {
     private Stack<String> paths;
+    private Stack<String> root;
     private Pattern pattern;
     private Matcher matcher;
 
     public Shell() {
         paths = new Stack<>();
+        root = new Stack<>();
         pattern = Pattern.compile("(\\/)*(\\w+)*");
     }
 
     public void cd(String path) {
-        if ((path.startsWith("..")) || (path.startsWith("/"))) {
+        if (path.startsWith("..")) {
+            root.add(paths.pop());
+        }
+        if (path.startsWith("/")) {
+            root.clear();
             paths.clear();
         }
         matcher = pattern.matcher(path);
